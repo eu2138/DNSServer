baf53ea53982e545824b9ddf0f975200207a9698
Eric Ung <eu2138@nyu.edu>
2025-10-23 00:58:10 -0500

    10232025-remove decode

--- DNSServer.py
+++ DNSServer.py
@@ -48,7 +48,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
-    return decrypted_data.decode()
+    return decrypted_data.decode('utf-8')
 
 #d_string = decrypt_with_aes(e_string, password, salt.encode('utf-8'))
 
@@ -103,7 +103,7 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: (encrypted_value.decode(),),
+        dns.rdatatype.TXT: (encrypted_value,),
     },
 
 

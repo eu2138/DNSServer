4b09f67b453475a169281eaced6e9de521a9c7ff
Eric Ung <eu2138@nyu.edu>
2025-10-22 19:13:53 -0500

    10222025-set dns flag Authority Answer manually

--- DNSServer.py
+++ DNSServer.py
@@ -86,10 +86,10 @@ dns_records = {
         ),
     },
    'safebank.com.': {
-        dns.rdatatype.A: '192.168.1.102'
+        dns.rdatatype.A: '192.168.1.102',
     },
     'google.com.': {
-        dns.rdatatype.A: '192.168.1.103'
+        dns.rdatatype.A: '192.168.1.103',
     },
     'legitsite.com.': {
         dns.rdatatype.A: '192.168.1.104',
@@ -102,7 +102,7 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.NS: 'ns1.nyu.edu',
-        dns.rdatatype.TXT: ('AlwaysWatching',),
+        dns.rdatatype.TXT: ('AlwaysWatching'),
     },
 
 
@@ -112,8 +112,8 @@ dns_records = {
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Research this
-    server_socket.bind(("127.0.0.1", 53))
-    #server_socket.bind(("", 50053))
+    #server_socket.bind(("127.0.0.1", 53))
+    server_socket.bind(("", 50053))
 
     while True:
         try:
@@ -155,6 +155,7 @@ def run_dns_server():
 
             # Set the response flags
             response.flags |= 1 << 10
+            response.flags |= dns.flags.AA
 
             # Send the response back to the client using the `server_socket.sendto` method and put the response to_wire(), return to the addr you received from
             print("Responding to request:", qname)
